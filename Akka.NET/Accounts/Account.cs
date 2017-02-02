54043868f00fe1bc180dfa3ca750ce72df0aee22
Vaughn Vernon <vvernon@shiftmethod.com>
2017-02-02 16:53:44 -0700

    Added Akka.NET examples.

---
+++ Akka.NET/Accounts/Account.cs
@@ -0,0 +1,134 @@
+﻿namespace Accounts
+{
+    using System;
+    using Akka;
+    using Akka.Actor;
+
+    // Commands
+    public class OpenAccount
+    {
+        public OpenAccount(string accountNumber, int initialBalance)
+        {
+            AccountNumber = accountNumber;
+            InitialBalance = initialBalance;
+        }
+
+        public string AccountNumber { get; private set; }
+        public int InitialBalance {  get; private set; }
+    }
+
+    public class DepositFunds
+    {
+        public DepositFunds(int amount)
+        {
+            Amount = amount;
+        }
+
+        public int Amount { get; private set; }
+    }
+
+    public class WithdrawFunds
+    {
+        public WithdrawFunds(int amount)
+        {
+            Amount = amount;
+        }
+
+        public int Amount { get; private set; }
+    }
+
+    // Events
+    public class AccountOpened
+    {
+        public AccountOpened(string accountNumber, int initialBalance)
+        {
+            AccountNumber = accountNumber;
+            InitialBalance = initialBalance;
+        }
+
+        public string AccountNumber { get; private set; }
+        public int InitialBalance { get; private set; }
+    }
+
+    public class FundsDeposited
+    {
+        public FundsDeposited(string accountNumber, int amount)
+        {
+            AccountNumber = accountNumber;
+            Amount = amount;
+        }
+
+        public string AccountNumber { get; private set; }
+        public int Amount { get; private set; }
+    }
+
+    public class FundsWithdrawn
+    {
+        public FundsWithdrawn(string accountNumber, int amount)
+        {
+            AccountNumber = accountNumber;
+            Amount = amount;
+        }
+
+        public string AccountNumber { get; private set; }
+        public int Amount { get; private set; }
+    }
+
+    public class AccountState
+    {
+        public AccountState(string accountNumber, int balance)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        internal string AccountNumber { get; private set; }
+        internal int Balance { get; private set; }
+
+        internal AccountState FromDeposited(int amount)
+        {
+            return new AccountState(AccountNumber, Balance + amount);
+        }
+
+        internal AccountState FromWithdrawn(int amount)
+        {
+            return new AccountState(AccountNumber, Balance - amount);
+        }
+    }
+
+    public class Account : TypedActor, IHandle<OpenAccount>, IHandle<DepositFunds>, IHandle<WithdrawFunds>
+    {
+        public Account()
+        {
+        }
+
+        public void Handle(OpenAccount open)
+        {
+            State = new AccountState(open.AccountNumber, open.InitialBalance);
+            var newEvent = new AccountOpened(open.AccountNumber, open.InitialBalance);
+            var answer = new Tuple<AccountState, AccountOpened>(State, newEvent);
+
+            Sender.Tell(answer, Sender);
+        }
+
+        public void Handle(DepositFunds deposit)
+        {
+            State = State.FromDeposited(deposit.Amount);
+            var newEvent = new FundsDeposited(State.AccountNumber, deposit.Amount);
+            var answer = new Tuple<AccountState, FundsDeposited>(State, newEvent);
+
+            Sender.Tell(answer, Sender);
+        }
+
+        public void Handle(WithdrawFunds withdraw)
+        {
+            State = State.FromWithdrawn(withdraw.Amount);
+            var newEvent = new FundsWithdrawn(State.AccountNumber, withdraw.Amount);
+            var answer = new Tuple<AccountState, FundsWithdrawn>(State, newEvent);
+
+            Sender.Tell(answer, Sender);
+        }
+
+        public AccountState State { get; private set; }
+    }
+}
---
+++ Akka.NET/Accounts/AccountTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Akka;
+using Akka.Actor;
+
+namespace Accounts
+{
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void TestAccountActor()
+        {
+            var system = ActorSystem.Create("acountContext");
+
+            var accountService = system.ActorOf<AccountService>();
+            accountService.Tell(new ManageAccount());
+
+            Thread.Sleep(5000);
+        }
+    }
+
+    public class ManageAccount { }
+
+    public class AccountService : TypedActor,
+        IHandle<ManageAccount>,
+        IHandle<Tuple<AccountState, AccountOpened>>,
+        IHandle<Tuple<AccountState, FundsDeposited>>,
+        IHandle<Tuple<AccountState, FundsWithdrawn>>
+    {
+        private IActorRef account = null;
+
+        public void Handle(ManageAccount message)
+        {
+            account = Context.ActorOf<Account>("A-1234");
+            account.Tell(new OpenAccount("A-1234", 100));
+        }
+
+        public void Handle(Tuple<AccountState, AccountOpened> message)
+        {
+            Console.WriteLine("AccountOpened: State: {0} and Event: {1}", message.Item1, message.Item2);
+            account.Tell(new DepositFunds(50));
+        }
+
+        public void Handle(Tuple<AccountState, FundsDeposited> message)
+        {
+            Console.WriteLine("FundsDeposited: State: {0} and Event: {1}", message.Item1, message.Item2);
+            account.Tell(new WithdrawFunds(75));
+        }
+
+        public void Handle(Tuple<AccountState, FundsWithdrawn> message)
+        {
+            Console.WriteLine("FundsWithdrawn: State: {0} and Event: {1}", message.Item1, message.Item2);
+        }
+    }
+}
