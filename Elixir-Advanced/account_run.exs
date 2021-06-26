e057351b75252dc6834b804341db4b10b44c3edc
everzet <ever.zet@gmail.com>
2021-06-26 15:09:59 +0100

    Make handling synchronous, hide aggregate state, add invariant protection
    Continuing further with opinionated changes, here's one where I hide
    aggregate state from its clients, exposing instead just events.
    
    I then make command handling a synchronous operation in order to
    introduce an explicit invariant protection. Although clients don't need
    to know the effect of a command on aggregates internal state, they do
    need to know if command succeeded or failed due to a failed invariant.
    
    I added support for invariant handling in the command handlers with an
    example (via unit test) of withdrawing funds beyond what's available in
    account.
    
    On top of that, I added support for command handler to be able to return
    multiple events. Which, in my experience, is a common occurrence.
    
    After this commit, the `Account` code is pretty close to how I would
    write my ES aggregate in Elixir.

--- Elixir-Advanced/account_run.exs
+++ Elixir-Advanced/account_run.exs
@@ -1,26 +1,8 @@
-defmodule Receiver do
-  def receive_result do
-    receive do
-      {state, event} ->
-        IO.inspect(event)
-        IO.inspect(state)
-        state
-    end
-  end
-
-  def receive_number_of_results(expected_number_of_results) do
-    unless expected_number_of_results == 0 do
-      receive_result()
-      receive_number_of_results(expected_number_of_results - 1)
-    end
-  end
+with {:ok, account} <- Account.start_link(),
+     {:ok, open_events} <- Account.open(account, account_number: "A-1234", initial_balance: 100),
+     {:ok, deposit_events} <- Account.deposit(account, amount: 50),
+     {:ok, withdraw_events} <- Account.withdraw(account, amount: 75) do
+  [open_events, deposit_events, withdraw_events]
+  |> List.flatten()
+  |> Enum.each(&IO.inspect/1)
 end
-
-{:ok, account} = Account.start_link()
-
-account
-|> Account.open(account_number: "A-1234", initial_balance: 100)
-|> Account.deposit(amount: 50)
-|> Account.withdraw(amount: 75)
-
-Receiver.receive_number_of_results(3)
