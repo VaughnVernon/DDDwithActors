f286be52c3fdaebf018ba5e16f3ebb9a9f13c111
everzet <ever.zet@gmail.com>
2021-06-26 10:07:16 +0100

    Reformat Elixir code with Mix Format
    Just executed `mix format *.{ex,exs}`
    
    Since 1.6 Elixir comes bundled with its own formatter. It is fully
    configurable, but also comes pretty opinionated out of the box. Most of
    the community (including myself) seem to like the default opinions as
    they drastically reduce the need to form and hold individual formatting
    preferences per each project.

--- Elixir/account_run.exs
+++ Elixir/account_run.exs
@@ -2,23 +2,26 @@ defmodule Receiver do
   def receive_result do
     receive do
       {state, event} ->
-        IO.puts event
-        IO.puts state
+        IO.puts(event)
+        IO.puts(state)
         state
     end
   end
 end
 
-account = Account.start
+account = Account.start()
 
-send account, {self(), %Account.Commands.OpenAccount{account_number: "A-1234", initial_balance: 100}}
+send(
+  account,
+  {self(), %Account.Commands.OpenAccount{account_number: "A-1234", initial_balance: 100}}
+)
 
-Receiver.receive_result
+Receiver.receive_result()
 
-send account, {self(), %Account.Commands.DepositFunds{amount: 50}}
+send(account, {self(), %Account.Commands.DepositFunds{amount: 50}})
 
-Receiver.receive_result
+Receiver.receive_result()
 
-send account, {self(), %Account.Commands.WithdrawFunds{amount: 75}}
+send(account, {self(), %Account.Commands.WithdrawFunds{amount: 75}})
 
-Receiver.receive_result
+Receiver.receive_result()
