a6545d4f88548822a4589a18b5a0ecacb3d1b045
everzet <ever.zet@gmail.com>
2021-06-26 11:10:45 +0100

    Add a smoke test
    Test can be run with `elixir account_test.exs`
    
    It effectively asserts that the `account_run.exs` produces the expected
    output. All the changes up until now were structural, but going forward
    I want to do couple behavioural changes that I wouldn't feel comfortable
    doing without an end-to-end test like this.

---
+++ Elixir/account_test.exs
@@ -0,0 +1,26 @@
+ExUnit.start(autorun: false)
+
+defmodule AccountTest do
+  use ExUnit.Case, async: true
+
+  Code.require_file("account.ex")
+
+  test "account_run.exs works" do
+    import ExUnit.CaptureIO
+
+    execute_script = fn ->
+      Code.require_file("account_run.exs")
+    end
+
+    assert capture_io(execute_script) =~ """
+           Account.AccountOpened account_number: A-1234, initial_balance: 100
+           Account.State account_number: A-1234, balance: 100
+           Account.FundsDeposited amount: 50, balance: 150
+           Account.State account_number: A-1234, balance: 150
+           Account.FundsWithdrawn amount: 75, balance: 75
+           Account.State account_number: A-1234, balance: 75
+           """
+  end
+end
+
+ExUnit.run()
